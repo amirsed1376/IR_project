486affbb14db49a09f0ce66abb8ce07656d8acf3
AmirReza Seddighin <amirreza.seddighin1376@gmail.com>
2020-07-21 22:24:07 +0430

    publish time

--- indexing.py
+++ indexing.py
@@ -2,27 +2,38 @@ from elasticsearch import Elasticsearch
 import os
 import json
 import requests
-
-
-def get_address_list(address):
-    result_list = list()
-    for root, dirs, files in os.walk(os.path.abspath(address)):
-        for file in files:
-            result_list.append(os.path.join(root, file))
-    return result_list
+import pandas as pd
+from needed import SqlManager, get_address_list   ,convert_time
 
 
 class CoronaIndexElasticSearch():
-    def __init__(self, host):
+    def __init__(self, host, sql_file="information.sqlite", index_name="corona_index", ):
         self.client = Elasticsearch(hosts=[host])
-        self.index_name = "corona_index"
-        self.url="http://"+host+"/"
-        self.create_index("corona_index")
+        self.index_name = index_name
+        self.url = "http://" + host + "/"
+        self.create_index(index_name)
+        self.sql_manager = SqlManager(sql_file)
+
+    def meta_data_pre_process(self, df):
+        df["publish_time"] = df["publish_time"].apply(lambda x: convert_time(x))
+        print(df["publish_time"])
+        # df["publish_time"]=strptime(df["publish_time"].s,'%b').tm_mon
+        return df
 
     def save_to_elasticsearch(self, address_list):
         for index, address in enumerate(address_list):
             data = dict(json.load(open(address, "r")))
-            self.client.index(index=self.index_name, body=data)
+            sha = str(address).split("/")[-1].split(".")[0].strip()
+            sql = "select publish_time from information where sha = '{}'".format(sha)
+            try:
+                result = self.sql_manager.crs.execute(sql).fetchall()[0][0]
+            except:
+                result = None
+            try:
+                data["publish_time"] = result
+                self.client.index(index=self.index_name, body=data)
+            except Exception as e:
+                print(e)
 
     def delete_index_from_elasticsearch(self):
         self.client.indices.delete(index=self.index_name)
@@ -58,24 +69,27 @@ class CoronaIndexElasticSearch():
         print("{} index created".format(index_name))
 
     def update_index_settings(self, settings: json):
-        print(self.url)
         response = requests.put(self.url + self.index_name + "/_settings/",
                                 headers={"Content-Type": "application/json"},
                                 data=settings)
         if json.loads(response.text).get("error", None) is not None:
-            res1 = requests.post(self.url + self.index_name + "/_close")
-            print(res1.text)
-            response = requests.put(self.url + self.index_name + "/_settings/",
-                                    headers={"Content-Type": "application/json"},
-                                    data=settings)
-            print(response.text)
-            res2 = requests.post(self.url + self.index_name + "/_open")
-            print(res2.text)
+            requests.post(self.url + self.index_name + "/_close")
+            requests.put(self.url + self.index_name + "/_settings/",
+                         headers={"Content-Type": "application/json"},
+                         data=settings)
+            requests.post(self.url + self.index_name + "/_open")
+
+    def meta_data_to_sql(self, address):
+        df = pd.read_csv(address)
+        columns = [col.replace(" ", "_") for col in df.columns]
+        df.columns = columns
+        meta_df = self.meta_data_pre_process(df=df)
+        meta_df.to_sql(name="information", con=self.sql_manager.conn, if_exists="replace")
 
 
 if __name__ == '__main__':
     elastic_Search = CoronaIndexElasticSearch("localhost:9200")
-    print(elastic_Search.url)
     addresses = get_address_list("cord-19_2020-03-13/2020-03-13/jsons")
     # elastic_Search.save_to_elasticsearch(address_list=addresses)
+    elastic_Search.meta_data_to_sql(address="cord-19_2020-03-13/2020-03-13/all_sources_metadata_2020-03-13.csv")
     # elastic_Search.delete_index_from_elasticsearch()
---
+++ needed.py
@@ -0,0 +1,37 @@
+
+import sqlite3
+import os
+from time import strptime
+
+
+class SqlManager:
+    def __init__(self, file):
+        self.conn = sqlite3.connect(file)
+        self.crs = self.conn.cursor()
+
+
+def create_folder(address):
+    if not os.path.exists(address):
+        os.makedirs(address)
+
+
+def get_address_list(address):
+    result_list = list()
+    for root, dirs, files in os.walk(os.path.abspath(address)):
+        for file in files:
+            result_list.append(os.path.join(root, file))
+    return result_list
+
+
+def convert_time(time):
+    time = str(time).strip()
+    if " " in time:
+        try:
+            splits = time.split(" ")
+            return "{}-{}-{}".format(splits[0].strtip(), strptime(splits[1].strtip(), '%b').tm_mon, splits[2])
+        except:
+            return None
+    elif "-" in time:
+        return time
+    else:
+        return "{}-01-01".format(time)
